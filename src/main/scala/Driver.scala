a7f781a95e45c1ebbe4956b3e7bb33cc27a890fb
dt27182 <dt27182@gmail.com>
2016-04-26 19:20:00 -0700

    add more tests

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -8,15 +8,16 @@ import Chisel.internal.firrtl.Component
 
 
 object Driver extends App {
-  val s = Chisel.Driver.emit(() => new Hello())
+  val s = Chisel.Driver.emit(() => new SampleDUT())
   val rootDir = new File(".").getCanonicalPath()
-  val dut = "Hello"
+  //val dut = "Hello"
+  val dut = "SampleDUT"
   val buildDir = s"${rootDir}/build"
   val verilogFile = s"${buildDir}/${dut}.v"
   val cppHarness = s"${rootDir}/chisel3/src/main/resources/top.cpp"
 
   // Run Chisel 3
-  val a = Chisel.Driver.elaborateModule(() => new Hello())
+  val a = Chisel.Driver.elaborateModule(() => new SampleDUT())
   println(s)
 
   // Parse circuit into FIRRTL
@@ -26,5 +27,5 @@ object Driver extends App {
   // Compile to verilog
   firrtl.VerilogCompiler.run(circuit, writer)
   writer.close()
-  runClassicTester(() => new Hello(), verilogFile) {c => new HelloTester(c)}
+  runClassicTester(() => new SampleDUT(), verilogFile) {c => new SampleDUTTester(c)}
 }
