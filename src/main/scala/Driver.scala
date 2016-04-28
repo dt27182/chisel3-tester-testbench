1f92f4d612683586e4355077a43eb18fe2b334bc
dt27182 <dt27182@gmail.com>
2016-04-28 08:57:40 -0700

    add test benches

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -3,29 +3,31 @@ import Chisel.hwiotesters._
 import firrtl._
 import java.io._
 
-import Chisel.internal.firrtl.Component
+object genVerilog {
+  def apply(dutGen: ()=> Chisel.Module, dutName: String) = {
+    val dutModule = Chisel.Driver.emit(dutGen)
+    val rootDir = new File(".").getCanonicalPath()
+    val buildDir = s"${rootDir}/build"
+    val verilogFilePath = s"${buildDir}/${dutName}.v"
 
+    // Parse circuit into FIRRTL
+    val circuit = firrtl.Parser.parse(dutModule.split("\n"))
 
+    val writer = new PrintWriter(new File(verilogFilePath))
+    // Compile to verilog
+    firrtl.VerilogCompiler.run(circuit, writer)
+    writer.close()
 
+    verilogFilePath
+  }
+}
 object Driver extends App {
-  val s = Chisel.Driver.emit(() => new SampleDUT())
-  val rootDir = new File(".").getCanonicalPath()
-  //val dut = "Hello"
-  val dut = "SampleDUT"
-  val buildDir = s"${rootDir}/build"
-  val verilogFile = s"${buildDir}/${dut}.v"
-  val cppHarness = s"${rootDir}/chisel3/src/main/resources/top.cpp"
-
-  // Run Chisel 3
-  val a = Chisel.Driver.elaborateModule(() => new SampleDUT())
-  println(s)
-
-  // Parse circuit into FIRRTL
-  val circuit = firrtl.Parser.parse(s.split("\n"))
-
-  val writer = new PrintWriter(new File(verilogFile))
-  // Compile to verilog
-  firrtl.VerilogCompiler.run(circuit, writer)
-  writer.close()
-  runClassicTester(() => new SampleDUT(), verilogFile) {c => new SampleDUTTester(c)}
+  var verilogFilePath = genVerilog(() => new SampleDUT(), "SampleDUT")
+  //runClassicTester(() => new SampleDUT(), verilogFilePath) {c => new SampleDUTTester(c)}
+  //verilogFilePath = genVerilog(() => new Accumulator(), "Accumulator")
+  //runClassicTester(() => new Accumulator(), verilogFilePath) {c => new AccumulatorTests(c)}
+  //verilogFilePath = genVerilog(() => new LFSR16(), "LFSR16")
+  //runClassicTester(() => new LFSR16(), verilogFilePath) {c => new LFSR16Tests(c)}
+  verilogFilePath = genVerilog(() => new VendingMachine(), "VendingMachine")
+  runClassicTester(() => new VendingMachine(), verilogFilePath) {c => new VendingMachineTests(c)}
 }
